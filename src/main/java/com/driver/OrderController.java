787759f52e290febc7f48a6fc5196f25d0c9b8eb
PRedAfTanIK <predaftanik0@gmail.com>
2023-03-31 11:32:32 +0530

    Amaze Ass - Sub 2

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("order")
 public class OrderController {
     @Autowired
     OrderService service;
